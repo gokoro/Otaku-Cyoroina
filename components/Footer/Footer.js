a99136803b3f9f510fb4e10380b8b3ddd5e08d24
Gokoro <raycle1004@naver.com>
2020-06-13 15:56:10 +0900

    Update the repo url of footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => (
     <div className={styles.footer}>
-        <a href="https://github.com/gokoro">The Code is made by Gokoro</a>
+        <a href="https://github.com/gokoro/Otaku-Cyoroina">The Code is made by Gokoro</a>
     </div>
 )
 
